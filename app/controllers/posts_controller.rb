12847b5c2bdc68dddce341d1f831e9d28b18273b
SAI <sai@SAIdeMacBook-Pro.local>
2017-03-29 23:04:20 +0800

    My Posts edit/update/destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,31 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path(@group), notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:alert] = "删除成功"
+      redirect_to account_posts_path
+    end
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
